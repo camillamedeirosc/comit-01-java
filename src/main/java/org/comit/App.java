510f4f011bc9beb4e30cd8daabe79dedcbd62d5b
camillamedeirosc <camilla.medeirosc@gmail.com>
2022-06-30 23:49:57 -0600

    first commit

---
+++ src/main/java/org/comit/App.java
@@ -0,0 +1,60 @@
+package org.comit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    /*
+     * 1. Primitives
+     * 2. Class Variables (Wrapper Classes)
+     */
+    /*
+     * a. Numeric Variables
+     * 
+     * Integers (Numbers that do not have decimals)
+     * 
+     */
+    	
+    	int ap = 2_000_000_000;
+    	Integer ac = 10;
+    	
+    	long lp = 2_000_000_000_000L;
+    	Long lc =10L; 
+    	
+    	short sp = 30_000;
+    	Short sc = 20_000;
+    	
+    	byte bp = 127;
+    	Byte bc = -127;
+    	
+    	/*
+    	 * Decimals
+    	 */
+    	
+    	double dp = 10.4536; /*8bytes*/
+    	Double dc = 10.4524;
+    	
+    	float fp = 10.6F; /* 4bytes */
+    	Float fc = 10.4F;
+    	
+    	/*
+    	 * Text variables
+    	 */
+    	
+    	char cp = 'E';
+    	Character cc = 'E';
+    	
+    	String str = "I love comIT";
+    	
+    	//var ax = 4;
+    	
+    	
+    	
+    	
+    	
+    }
+}
